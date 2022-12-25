0e59ac90f4f702d94a61cd1a0f7ed525585cd95f
MichalIzb <michalizb@wp.pl>
2022-12-25 15:30:35 +0100

    component-created-and-bootstrap-icons-added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AwardComponent } from './award/award.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyPipe
+    MyPipe,
+    AwardComponent
   ],
   imports: [
     BrowserModule,
