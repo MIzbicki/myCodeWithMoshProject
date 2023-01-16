0e22b10bdfc2329dc1c43e9e0461efc39f7daa90
MichalIzb <michalizb@wp.pl>
2023-01-16 20:30:29 +0100

    everything works correctly

--- src/app/udemy-form/udemy-form.component.ts
+++ src/app/udemy-form/udemy-form.component.ts
@@ -16,4 +16,10 @@ export class UdemyFormComponent implements OnInit {
     console.log(form);
   }
 
+  categories = [
+    { id: 1, name: 'Development' },
+    { id: 2, name: 'Art' },
+    { id: 3, name: 'Languages' }
+  ]
+
 }
