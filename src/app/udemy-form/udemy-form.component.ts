23019c1f85f1ab71892442a012eef0bc392768eb
MichalIzb <michalizb@wp.pl>
2023-01-16 19:53:43 +0100

    component generated

---
+++ src/app/udemy-form/udemy-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-udemy-form',
+  templateUrl: './udemy-form.component.html',
+  styleUrls: ['./udemy-form.component.css']
+})
+export class UdemyFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
