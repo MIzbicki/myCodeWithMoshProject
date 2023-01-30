1c825f02850b8c712fd353c07e7c2b278189dabf
MichalIzb <michalizb@wp.pl>
2023-01-30 21:14:35 +0100

    http post works correctly

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,3 +1,4 @@
+import { ValidationErrors } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
@@ -9,21 +10,46 @@ import { map } from 'rxjs/operators';
 })
 export class PostComponent implements OnInit {
 
-  posts: any[] = [];
+  posts: any = [];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
+  constructor(private http: HttpClient){
+    http.get(this.url)
+    .subscribe(response => {
+      this.posts = response;
+      console.log(typeof response);
+    });
+  }
+
+  addToList(myInput: any){
+    let post = { title: myInput.value };
+    myInput.value='';
+
+    this.http.post(this.url, post)
+    .subscribe(response => {
+      console.log(response);
+      this.posts.splice(0,0, response);
+    });
+
+  }
+
+
+  /*
+  //works correctly
   constructor(private http: HttpClient) {
     this.http.get<any>('https://jsonplaceholder.typicode.com/posts')
       .pipe(
         map(response => response)
       ).subscribe(response => {
         this.posts = (response);
+        console.log(typeof response);
         console.log(this.posts);
       });
   }
-
-
+  */
 
   /*
+  //doesn't works
   constructor(http: Http) {
     http.get('https://jsonplaceholder.typicode.com/posts')
       .subscribe(response => {
